f5da690a62f3d748c0e22d91d455d8c7665bbd9d
Sohan1928 <sohanurrahman1928@gmail.com>
2025-04-16 21:48:40 +0800

    project setup complete

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/home/navbar/Navbar";
+
+function App() {
+  return (
+    <section>
+      <div className="w-[1600px] mx-auto">
+        <Navbar></Navbar>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/home/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <section className="md:px-[40px] lg:px-[80px] pt-[30px]">
+      <div>
+        <h3>This is navbar components</h3>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
