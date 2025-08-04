dacf1caa4d9c7ef13691c0206fbbf8da55fb7cfd
Sohan1928 <sohanurrahman1928@gmail.com>
2025-08-04 19:58:54 +0600

    milestones section updated

--- src/components/home/milestones/Milestones.tsx
+++ src/components/home/milestones/Milestones.tsx
@@ -1,7 +1,7 @@
 const Milestones = () => {
   return (
     <section className="milestones-container pl-[90px] pt-[30px] pb-[90px]">
-      <div className="container-content flex items-center justify-between gap-[70px]">
+      <div className="container-content flex items-center gap-[80px]">
         <div className="title-section">
           <h5 className="subtitle">Milestones</h5>
           <h2 className="title">Titano Highlights and Milestones</h2>
@@ -17,7 +17,23 @@ const Milestones = () => {
             media platforms. Come Join Us!
           </p>
         </div>
-        <div className="audited-section"></div>
+        <div className="audited-section">
+          <h4>Audited</h4>
+          <p className="pt-5">
+            Titano’s contracts have been audited by Solidity Finance with no
+            major issues discovered. We are currently under audit by CerTik, the
+            industry’s top auditor.
+          </p>
+          <h4 className="pt-[65px]">Weekly Token Burning</h4>
+          <p className="pt-[23px]">
+            Each week Titano burns nearly.5% of its locked tokens creating
+            inflation stability.
+          </p>
+          <h4 className="pt-[96px]">Liquidity Locked</h4>
+          <p className="pt-[23px]">
+            90% of Titano’s total token supply is vested into Unicrypt.
+          </p>
+        </div>
       </div>
     </section>
   );
