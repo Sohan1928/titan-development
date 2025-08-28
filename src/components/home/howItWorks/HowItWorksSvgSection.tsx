709a79d34e70f34c4ee67a537a20d4d156c50ed0
Sohan1928 <sohanurrahman1928@gmail.com>
2025-08-28 20:07:00 +0600

    small device responsive code updated

--- src/components/home/howItWorks/HowItWorksSvgSection.tsx
+++ src/components/home/howItWorks/HowItWorksSvgSection.tsx
@@ -39,151 +39,151 @@ const HowItWorksSvgSection = () => {
 
   return (
     <section className="howItWorksSvgSectionContainer">
-      <div className="">
-        <div className="relative">
-          <h4 className="theAutomaticText z-50">
-            <span>The Automatic Stacking,</span> and Compounding Titaton Token
-          </h4>
-          <h4 className="works-titan-icon-svg flex flex-col items-center justify-center">
-            <TitanIconSvg></TitanIconSvg>
-          </h4>
-          {/* top box svg */}
-          <div className="how-it-svg-section absolute -top-64 left-1/2 transform -translate-x-1/2">
-            <div className="relative">
-              <div className="top-box-svg">
-                <TopBoxSvg></TopBoxSvg>
-              </div>
-              {/* Text Overlay */}
-              <div className="pancake-swap-section inset-0 flex items-center justify-center">
-                <p className="flex items-center justify-center gap-1 font-bold text-white">
-                  <div className="pancake-swap-icon-svg">
-                    <PancakeSwapIconSvg></PancakeSwapIconSvg>
-                  </div>
-                  <h6 className="pancake-swap-text">PancakeSwap</h6>
+      <div className="relative">
+        <h4 className="theAutomaticText z-50">
+          <span>The Automatic Stacking,</span> and Compounding Titaton Token
+        </h4>
+        <h4 className="works-titan-icon-svg flex flex-col items-center justify-center">
+          <TitanIconSvg></TitanIconSvg>
+        </h4>
+        {/* top box svg */}
+        <div className="how-it-svg-section absolute -top-64 left-1/2 transform -translate-x-1/2">
+          <div className="relative">
+            <div className="top-box-svg">
+              <TopBoxSvg></TopBoxSvg>
+            </div>
+            {/* Text Overlay */}
+            <div className="pancake-swap-section inset-0 flex items-center justify-center">
+              <p className="flex items-center justify-center gap-1 font-bold text-white">
+                <div className="pancake-swap-icon-svg">
+                  <PancakeSwapIconSvg></PancakeSwapIconSvg>
+                </div>
+                <h6 className="pancake-swap-text">PancakeSwap</h6>
+              </p>
+            </div>
+            {/* circle ellips */}
+            <div className="how-it-right-ellipse-svg">
+              <RightSideEllipseSvg></RightSideEllipseSvg>
+            </div>
+            {/* circle ellips */}
+            <div className="how-it-left-ellipse-svg">
+              <RightSideEllipseSvg></RightSideEllipseSvg>
+            </div>
+            {/* circle ellips */}
+            <div className="how-it-bottom-ellipse-svg">
+              <RightSideEllipseSvg></RightSideEllipseSvg>
+            </div>
+            {/* bottom line arrow */}
+            <div className="how-it-bottom-arrow">
+              <BottomArrowSvg></BottomArrowSvg>
+            </div>
+            {/* right side */}
+            {/* right line arrow 1 */}
+            <div className="how-it-right-line-arrow1">
+              <LineArrow1></LineArrow1>
+            </div>
+            {/* right line arrow 2 */}
+            <div className="how-it-right-line-arrow2">
+              <LineArrow2></LineArrow2>
+            </div>
+            {/* right side text */}
+            <div className="absolute sm:-right-[140px] sm:top-0 lg:-right-[150px] lg:top-6">
+              <h4 className="buyAndSell uppercase">Buy & Sell Fees</h4>
+            </div>
+            {/* right side box */}
+            <div className="how-it-right-box z-50">
+              <RightBoxSvg></RightBoxSvg>
+              <div className="howItWorksMiddleBoxContent how-it-works-right-middle-box z-50 text-center">
+                <h3 className="mb-[10px]">Titano Treasury</h3>
+                <p>
+                  A portion of the buy and sells fees goes to the treasury for
+                  marketing, new development and to support RFV
                 </p>
               </div>
-              {/* circle ellips */}
-              <div className="how-it-right-ellipse-svg">
-                <RightSideEllipseSvg></RightSideEllipseSvg>
-              </div>
-              {/* circle ellips */}
-              <div className="how-it-left-ellipse-svg">
-                <RightSideEllipseSvg></RightSideEllipseSvg>
-              </div>
-              {/* circle ellips */}
-              <div className="how-it-bottom-ellipse-svg">
-                <RightSideEllipseSvg></RightSideEllipseSvg>
-              </div>
-              {/* bottom line arrow */}
-              <div className="how-it-bottom-arrow">
-                <BottomArrowSvg></BottomArrowSvg>
-              </div>
-              {/* right side */}
-              {/* right line arrow 1 */}
-              <div className="how-it-right-line-arrow1">
-                <LineArrow1></LineArrow1>
-              </div>
-              {/* right line arrow 2 */}
-              <div className="how-it-right-line-arrow2">
-                <LineArrow2></LineArrow2>
-              </div>
-              {/* right side text */}
-              <div className="absolute -right-[150px] top-6">
-                <h4 className="buyAndSell uppercase">Buy & Sell Fees</h4>
-              </div>
-              {/* right side box */}
-              <div className="how-it-right-box z-50">
-                <RightBoxSvg></RightBoxSvg>
-                <div className="howItWorksMiddleBoxContent how-it-works-right-middle-box z-50 text-center">
-                  <h3 className="mb-[10px]">Titano Treasury</h3>
+            </div>
+            {/* right box bg */}
+            <div className="absolute z-0 -right-[430px] top-44 pointer-events-none">
+              <RightBoxBg></RightBoxBg>
+            </div>
+            {/* right box middle tick */}
+            <div className="how-it-right-box-tick z-50 pointer-events-none">
+              <RightBoxTick></RightBoxTick>
+            </div>
+
+            {/* left side  */}
+            {/* left line arrow 1 */}
+            <div className="how-it-left-line-arrow1">
+              <LineArrow1></LineArrow1>
+            </div>
+            {/* left line arrow 2 */}
+            <div className="how-it-left-line-arrow2">
+              <LineArrow2></LineArrow2>
+            </div>
+            {/* left side text */}
+            <div className="absolute sm:-left-[140px] sm:top-0 lg:-left-[150px] lg:top-6">
+              <h4 className="buyAndSell uppercase">Buy & Sell Fees</h4>
+            </div>
+            {/* left side box */}
+            <div className="how-it-left-box z-50">
+              <LeftBoxSvg></LeftBoxSvg>
+              <div className="">
+                <div className="howItWorksMiddleBoxContent how-it-works-left-middle-box z-50 text-center">
+                  <h3 className="mb-[10px]">Risk Free Value (RFV)</h3>
                   <p>
                     A portion of the buy and sells fees goes to the treasury for
                     marketing, new development and to support RFV
                   </p>
                 </div>
               </div>
-              {/* right box bg */}
-              <div className="absolute z-0 -right-[430px] top-44 pointer-events-none">
-                <RightBoxBg></RightBoxBg>
-              </div>
-              {/* right box middle tick */}
-              <div className="how-it-right-box-tick z-50 pointer-events-none">
-                <RightBoxTick></RightBoxTick>
-              </div>
+            </div>
+            {/* left box bg */}
+            <div className="absolute z-0 -left-[430px] top-44 pointer-events-none">
+              <RightBoxBg></RightBoxBg>
+            </div>
+            {/* left box middle tick */}
+            <div className="how-it-left-box-tick z-50 pointer-events-none">
+              <LeftSideBoxCircle></LeftSideBoxCircle>
+            </div>
 
-              {/* left side  */}
-              {/* left line arrow 1 */}
-              <div className="how-it-left-line-arrow1">
-                <LineArrow1></LineArrow1>
-              </div>
-              {/* left line arrow 2 */}
-              <div className="how-it-left-line-arrow2">
-                <LineArrow2></LineArrow2>
-              </div>
-              {/* left side text */}
-              <div className="absolute -left-[150px] top-6">
-                <h4 className="buyAndSell uppercase">Buy & Sell Fees</h4>
-              </div>
-              {/* left side box */}
-              <div className="how-it-left-box z-50">
-                <LeftBoxSvg></LeftBoxSvg>
-                <div className="">
-                  <div className="howItWorksMiddleBoxContent how-it-works-left-middle-box z-50 text-center">
-                    <h3 className="mb-[10px]">Risk Free Value (RFV)</h3>
-                    <p>
-                      A portion of the buy and sells fees goes to the treasury
-                      for marketing, new development and to support RFV
-                    </p>
-                  </div>
-                </div>
-              </div>
-              {/* left box bg */}
-              <div className="absolute z-0 -left-[430px] top-44 pointer-events-none">
-                <RightBoxBg></RightBoxBg>
-              </div>
-              {/* left box middle tick */}
-              <div className="how-it-left-box-tick z-50 pointer-events-none">
-                <LeftSideBoxCircle></LeftSideBoxCircle>
-              </div>
-
-              {/* bottom arrow and all svg setup */}
-              <div className="bottom-arrow-how-it absolute ">
-                <BottomArrow></BottomArrow>
-              </div>
-              <div className="flat-arrow-how-it">
-                <FlatArrow></FlatArrow>
-              </div>
-              <div className="small-arrow-how-it1">
-                <SmallArrow></SmallArrow>
-              </div>
-              <div className="small-arrow-how-it2">
-                <SmallArrow></SmallArrow>
-              </div>
-              {/* left box */}
-              <div className="how-it-bottom-left-box ">
-                <BottomBox></BottomBox>
-              </div>
-              <div className="howItWorksBottomBoxContent how-it-bottom-left-box-text">
-                <h4 className="text-center">
-                  You receive <span>,03959%</span> APY in your wallet every 30
-                  minutes
-                </h4>
-              </div>
-              {/* right box */}
-              <div className="how-it-bottom-right-box">
-                <BottomBox></BottomBox>
-              </div>
-              <div className="howItWorksBottomBoxContent how-it-bottom-right-box-text">
-                <h4 className="text-center">
-                  You receive <span>,03959%</span> APY in your wallet every 30
-                  minutes
-                </h4>
-              </div>
+            {/* bottom arrow and all svg setup */}
+            <div className="bottom-arrow-how-it absolute ">
+              <BottomArrow></BottomArrow>
+            </div>
+            <div className="flat-arrow-how-it">
+              <FlatArrow></FlatArrow>
+            </div>
+            <div className="small-arrow-how-it1">
+              <SmallArrow></SmallArrow>
+            </div>
+            <div className="small-arrow-how-it2">
+              <SmallArrow></SmallArrow>
+            </div>
+            {/* left box */}
+            <div className="how-it-bottom-left-box ">
+              <BottomBox></BottomBox>
+            </div>
+            <div className="howItWorksBottomBoxContent how-it-bottom-left-box-text">
+              <h4 className="text-center">
+                You receive <span>,03959%</span> APY in your wallet every 30
+                minutes
+              </h4>
+            </div>
+            {/* right box */}
+            <div className="how-it-bottom-right-box">
+              <BottomBox></BottomBox>
+            </div>
+            <div className="howItWorksBottomBoxContent how-it-bottom-right-box-text">
+              <h4 className="text-center">
+                You receive <span>,03959%</span> APY in your wallet every 30
+                minutes
+              </h4>
             </div>
           </div>
-          <div className="absolute z-90 -left-[650px] top-[300px] -translate-y-1/2">
-            <h2 className="rotateText stroke-text">TITANO AUTOTASK PROTOCOL</h2>
-          </div>
+        </div>
+        <div className="z-90 -translate-y-1/2">
+          <h2 className="rotateText stroke-text how-it-rotate-text">
+            TITANO AUTOTASK PROTOCOL
+          </h2>
         </div>
       </div>
     </section>
