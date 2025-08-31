f46f2f3b92efb9bc3c698306a22a99dae9522c2d
Sohan1928 <sohanurrahman1928@gmail.com>
2025-08-31 15:42:56 +0600

    milestones container responsive code updated

--- src/components/home/milestones/Milestones.tsx
+++ src/components/home/milestones/Milestones.tsx
@@ -2,70 +2,70 @@ import { ScrollTrigger } from "gsap/all";
 import Roadmap from "./Roadmap";
 import MilestonesBottomRightSvg from "./svg/MilestonesBottomRightSvg";
 import MilestonesTopLeftSvg from "./svg/MilestonesTopLeftSvg";
-import { useGSAP } from "@gsap/react";
+// import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Milestones = () => {
-  useGSAP(() => {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".container-content",
-        start: "top 70%",
-        end: "top 10%",
-        scrub: true,
-      },
-    });
-    tl.from(".container-content .title-section > *", {
-      opacity: 0,
-      scale: 0.7,
-      y: 50,
-      x: -50,
-      duration: 1.6,
-      ease: "power3.out",
-      stagger: 0.3,
-    });
-  });
-  useGSAP(() => {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".container-content",
-        start: "top 60%",
-        end: "top 10%",
-        scrub: true,
-      },
-    });
-    tl.from(".container-content .audited-section > *", {
-      opacity: 0,
-      scale: 0.7,
-      y: 50,
-      x: 50,
-      duration: 0.8,
-      ease: "power3.out",
-      stagger: 0.3,
-    });
-  });
-  useGSAP(() => {
-    gsap.from(".milestones-container .stroke-text", {
-      opacity: 0,
-      scale: 0.7,
-      x: 100,
-      duration: 1.4,
-      ease: "sine.out",
-      stagger: 0.3,
-      scrollTrigger: {
-        trigger: ".rotate-titano-roadmap",
-        start: "top 70%",
-        end: "top 10%",
-        scrub: true,
-      },
-    });
-  });
+  // useGSAP(() => {
+  //   const tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: ".container-content",
+  //       start: "top 70%",
+  //       end: "top 10%",
+  //       scrub: true,
+  //     },
+  //   });
+  //   tl.from(".container-content .title-section > *", {
+  //     opacity: 0,
+  //     scale: 0.7,
+  //     y: 50,
+  //     x: -50,
+  //     duration: 1.6,
+  //     ease: "power3.out",
+  //     stagger: 0.3,
+  //   });
+  // }, []);
+  // useGSAP(() => {
+  //   const tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: ".container-content",
+  //       start: "top 60%",
+  //       end: "top 10%",
+  //       scrub: true,
+  //     },
+  //   });
+  //   tl.from(".container-content .audited-section > *", {
+  //     opacity: 0,
+  //     scale: 0.7,
+  //     y: 50,
+  //     x: 50,
+  //     duration: 0.8,
+  //     ease: "power3.out",
+  //     stagger: 0.3,
+  //   });
+  // }, []);
+  // useGSAP(() => {
+  //   gsap.from(".milestones-container .stroke-text", {
+  //     opacity: 0,
+  //     scale: 0.7,
+  //     x: 100,
+  //     duration: 1.4,
+  //     ease: "sine.out",
+  //     stagger: 0.3,
+  //     scrollTrigger: {
+  //       trigger: ".rotate-titano-roadmap",
+  //       start: "top 70%",
+  //       end: "top 10%",
+  //       scrub: true,
+  //     },
+  //   });
+  // }, []);
 
   return (
     <section className="milestones-container mt-[100vh] relative">
-      <div className="container-content flex items-center justify-center gap-[90px] z-50">
+      <div className="container-content z-50">
         <div className="title-section">
           <h5 className="subtitle">Milestones</h5>
           <div className="relative">
